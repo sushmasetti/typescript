61b70ab3b45169f7bcf20ce97f51dd2e74945e5c
unknown <k.sushmasetti1@gmail.com>
2019-08-23 09:05:46 -0700

    adfh

--- movie.ts
+++ movie.ts
@@ -3,7 +3,6 @@ class Movie
 {
 public hero:String="Rajini";
 
-
 }
 class Movie1 extends Movie{
 public director:String="shankar";
--- terneryoperatory.js
+++ terneryoperatory.js
@@ -1,6 +1,3 @@
-var v = 5;
-var b = 10;
-console.log(v > b ? "a is bigger" : "b is bigger");
 var v = 15;
 var b = 10;
 console.log(v > b ? "a is bigger" : "b is bigger");
